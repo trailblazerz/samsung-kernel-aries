30507356de7ff0f75f2af710dc04e17b3b7efde7
trailblazerz <juston@uoregon.edu>
2012-04-27 15:51:25 -0700

    update

--- tbuild.sh
+++ tbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # CM9 repo path :
 ANDROID_BUILD_TOP=~/CM9
-sema_ver="Semaphore_ICS_1.0.0sbm_Vibrant"
+sema_ver="Semaphore_ICS_1.1.1sbm_Vibrant"
 export LOCALVERSION="-"`echo $sema_ver`
 
 setup ()
