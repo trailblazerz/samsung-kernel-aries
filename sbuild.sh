55363985f051c05911f69dbe2e6487c0739298cf
trailblazerz <juston@uoregon.edu>
2012-05-18 17:09:41 -0700

    clean up

--- sbuild.sh
+++ sbuild.sh
@@ -5,6 +5,7 @@ sema_ver="Semaphore_ICS_1.2.0s_Vibrant_CFS"
 #export KBUILD_BUILD_VERSION="2"
 export LOCALVERSION="-"`echo $sema_ver`
 
+#make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm aries_vibrantmtd_defconfig
 #make CROSS_COMPILE=/opt/toolchains/android-toolchain-eabi-12.01/bin/arm-eabi- ARCH=arm -j4
 #make CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi- ARCH=arm -j4
 make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm -j8 modules
