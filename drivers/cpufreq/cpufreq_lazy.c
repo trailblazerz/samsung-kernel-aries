bbbe9cbe6639daa6807bf5f76c209c38ac4b06a6
trailblazerz <juston@uoregon.edu>
2012-06-01 22:14:13 -0700

    update lazy

--- drivers/cpufreq/cpufreq_lazy.c
+++ drivers/cpufreq/cpufreq_lazy.c
@@ -35,6 +35,7 @@
 
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(10)
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#define DEF_SAMPLE_RATE				(15000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(90)
 #define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(10000)
@@ -299,6 +300,7 @@ static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
     if (ret != 1)
 	return -EINVAL;
     dbs_tuners_ins.sampling_rate = max(input, min_sampling_rate);
+    dbs_tuners_ins.min_timeinstate = max(dbs_tuners_ins.min_timeinstate, dbs_tuners_ins.sampling_rate);
     return count;
 }
 
@@ -390,7 +392,7 @@ static ssize_t store_min_timeinstate(struct kobject *a, struct attribute *b,
     ret = sscanf(buf, "%u", &input);
     if (ret != 1)
 	return -EINVAL;
-    dbs_tuners_ins.min_timeinstate = max(input, min_sampling_rate);
+    dbs_tuners_ins.min_timeinstate = max(input, dbs_tuners_ins.sampling_rate);
     return count;
 }
 
@@ -731,9 +733,10 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 	    /* Bring kernel and HW constraints together */
 	    min_sampling_rate = max(min_sampling_rate,
 				    MIN_LATENCY_MULTIPLIER * latency);
-	    dbs_tuners_ins.sampling_rate = min_sampling_rate;
+	    dbs_tuners_ins.sampling_rate = max(min_sampling_rate, DEF_SAMPLE_RATE);
 	    current_sampling_rate = dbs_tuners_ins.sampling_rate;
 	    dbs_tuners_ins.min_timeinstate = latency * LATENCY_MULTIPLIER;
+	    dbs_tuners_ins.min_timeinstate = max(dbs_tuners_ins.sampling_rate, dbs_tuners_ins.min_timeinstate);
 	    dbs_tuners_ins.io_is_busy = should_io_be_busy();
 	}
 	mutex_unlock(&dbs_mutex);
