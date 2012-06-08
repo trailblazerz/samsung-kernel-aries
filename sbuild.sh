eb2cea3fb1e426f77d44bbb0c73cab40b08d9060
trailblazerz <juston@uoregon.edu>
2012-06-07 22:23:01 -0700

    clean up and streamline build script config in arch/arm/configs

--- sbuild.sh
+++ sbuild.sh
@@ -2,16 +2,13 @@
 
 sema_ver="Semaphore_ICS_1.2.2s_Vibrant"
 
-export KBUILD_BUILD_VERSION="6"
+export KBUILD_BUILD_VERSION="7"
 export LOCALVERSION="-"`echo $sema_ver`
 
-#make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm aries_vibrantmtd_defconfig
-#make CROSS_COMPILE=/opt/toolchains/android-toolchain-eabi-12.01/bin/arm-eabi- ARCH=arm -j4
-#make CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi- ARCH=arm -j4
+make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm vibrant_sema_defconfig
+
 make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm -j8 modules
 
-
-#find /kernels/samsung-kernel-aries/ -name '*.ko' -exec cp -v {} /kernels/ics-ramdisk/cwm/system/lib/modules \;
 find /home/juston/CM9 -name '*.ko' -exec cp -v {} /home/juston/CM9/kernel/samsung/stock/ics-ramdisk/ics_combo/files/modules \;
 
 make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm -j8 zImage
