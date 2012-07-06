5681d0c605e9339925569cf0fd6e7379d0d74676
trailblazerz <juston@uoregon.edu>
2012-07-06 13:47:16 -0700

    update linaro 4.7.1-06

--- sbuild.sh
+++ sbuild.sh
@@ -2,26 +2,26 @@
 
 START=$(date +%s)
 
-sema_ver="Semaphore_1.2.5s_Vibrant"
+sema_ver="Semaphore_1.2.6s_Vibrant"
 
-export KBUILD_BUILD_VERSION="2"
+export KBUILD_BUILD_VERSION="1"
 export LOCALVERSION="-"`echo $sema_ver`
 
 cp /home/juston/Desktop/built-in.o drivers/misc/samsung_modemctl
 
 #make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm vibrant_sema_defconfig
 
-make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.05-20120523_linux/bin/arm-linux-gnueabihf- ARCH=arm vibrant_sema_defconfig
+make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm vibrant_sema_defconfig
 
 #make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm -j8 modules
 
-make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.05-20120523_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 modules
+make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 modules
 
 find /home/juston/CM9 -name '*.ko' -exec cp -v {} /home/juston/CM9/kernel/samsung/stock/ics-ramdisk/ics_combo/files/modules \;
 
 #make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm -j8 zImage
 
-make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.05-20120523_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 zImage
+make CROSS_COMPILE=/home/juston/CM9/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 zImage
 
 cd arch/arm/boot
 tar cvf `echo $sema_ver`.tar zImage
